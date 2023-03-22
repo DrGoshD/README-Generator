9cce4d9eb896454eb3037741884625173707210d
DrPeppr <md.purcaru@gmail.com>
2023-03-22 17:48:15 +0000

    added questions

--- index.js
+++ index.js
@@ -12,13 +12,50 @@ const questions = [
         message: 'What is the title of your project?',
     },
     {
-        type: ,
-        name: ,
-        message: ,
+        type: 'editor',
+        name: 'description',
+        message: 'Please provide a short description of your project.Do not forget to save when finished.',
     },
     {
-        type: ,
-        name: ,
-        message: ,
+        type: 'editor',
+        name: 'installation',
+        message: 'Please provide guidance on how to install the application. Always save when done.',
     },
-]
+    {
+        type: 'editor',
+        name: 'usage',
+        message: 'Please provide details about the use and steps on how to use this application. Always save.',
+    },
+    {
+        type: 'checkbox',
+        name: 'license',
+        message: 'What license would you like to give your application?',
+        choices: ['MIT License', 'Apache License', 'GPL License'],
+    },
+    {
+        type: 'editor',
+        name: 'contributing',
+        message: 'Please provide guidelines for developers who want to share their contributions. Save.',
+    },
+    {
+        type: 'editor',
+        name: 'tests',
+        message: 'Please provide testing guidance. Save!',
+    },
+    {
+        type: 'input',
+        name: 'gitHub',
+        message: 'What is your GitHub username?',
+    },
+    {
+        type: 'input',
+        name: 'email',
+        message: 'What is your email address?',
+    },
+];
+
+
+
+
+
+init();
