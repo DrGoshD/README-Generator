b179aedbd51ac6a0506dfb5c7808f109b2fec8d4
DrPeppr <md.purcaru@gmail.com>
2023-03-22 20:11:47 +0000

    markdown.js done

--- utils/markdown.js
+++ utils/markdown.js
@@ -11,12 +11,47 @@ function generateMarkdown(data) {
 
     return `# ${data.title}
 
-    
+    ${licenseBadge}
+
     ##Description
+
+    ${data.description}
 
     ## Table of Contents
 
+    - [Installation](#installation)
+    - [Usage](#usage)
+    - [License](#license)
+    - [Contributing](#contributing)
+    - [Tests](#tests)
+    - [Questions](#questions)
+
     ## Installation 
+
+    ${data.installation}
+
+    ## Usage
+
+    ${data.usage}
+
+    ## License
+
+    This project is licensed under the ${data.license} license.
+
+    ## Contributing
+
+    ${data.contributing}
+
+    ## Tests
+
+    ${data.tests}
+
+    ## Questions
+
+    If you have any questions or feedback, don't hesitate to contact me via email or GitHub.
+
+    - Email: ${data.email}
+    - GitHub: https://github.com/${data.github}
 
   `;
   }
